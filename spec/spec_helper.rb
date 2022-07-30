73d5c53665759d2582f3e348b95257efa3723ee3
Kazumasa Hirata <50480499+kzmshx@users.noreply.github.com>
2022-07-30 21:53:01 +0900

    Update spec_helper (#1)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require "try-ruby-mysql"
+require "try-ruby-mysql/try_ruby_mysql"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
--- spec/try-ruby-mysql/try_ruby_mysql_spec.rb
+++ spec/try-ruby-mysql/try_ruby_mysql_spec.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.describe TryRubyMysql do
+  specify "1 + 1 = 2" do
+    expect(1 + 1).to eq 2
+  end
 end
